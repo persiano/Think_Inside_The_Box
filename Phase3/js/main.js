487ae28930a6c9a789768ed1ad8e9c2c230812f1
Sina <sinaranjbar@gmail.com>
2018-10-20 14:51:32 +0200

    Add files via upload

---
+++ Phase3/js/main.js
@@ -0,0 +1,1017 @@
+$('#full').click(function() {
+    $('#partime').hide();
+	$('#fulltime').show();
+});
+
+$('#part').click(function() {
+    $('#fulltime').hide();
+    $('#partime').show();
+});
+
+<!-- FUNZIONI GRAFICO PART TIME -->
+
+$('#austriamale').mouseenter(function() {
+    $('#box').show();
+});
+
+$('#austriamale').mouseleave(function() {
+    $('#box').hide();
+});
+
+$('#austriafemale').mouseenter(function() {
+    $('#box1').show();
+});
+
+$('#austriafemale').mouseleave(function() {
+    $('#box1').hide();
+});
+
+$('#belgiummale').mouseenter(function() {
+    $('#box2').show();
+});
+
+$('#belgiummale').mouseleave(function() {
+    $('#box2').hide();
+});
+
+$('#belgiumfemale').mouseenter(function() {
+    $('#box3').show();
+});
+
+$('#belgiumfemale').mouseleave(function() {
+    $('#box3').hide();
+});
+
+
+$('#bulgmale').mouseenter(function() {
+    $('#box4').show();
+});
+
+$('#bulgmale').mouseleave(function() {
+    $('#box4').hide();
+});
+
+
+$('#bulgfemale').mouseenter(function() {
+    $('#box5').show();
+});
+
+$('#bulgfemale').mouseleave(function() {
+    $('#box5').hide();
+});
+
+
+$('#croatiamale').mouseenter(function() {
+    $('#box6').show();
+});
+
+$('#croatiamale').mouseleave(function() {
+    $('#box6').hide();
+});
+
+$('#croatiafemale').mouseenter(function() {
+    $('#box7').show();
+});
+
+$('#croatiafemale').mouseleave(function() {
+    $('#box7').hide();
+});
+
+
+$('#cyprusmale').mouseenter(function() {
+    $('#box8').show();
+});
+
+$('#cyprusmale').mouseleave(function() {
+    $('#box8').hide();
+});
+
+$('#cyprusfemale').mouseenter(function() {
+    $('#box9').show();
+});
+
+$('#cyprusfemale').mouseleave(function() {
+    $('#box9').hide();
+});
+
+
+$('#czechmale').mouseenter(function() {
+    $('#box10').show();
+});
+
+$('#czechmale').mouseleave(function() {
+    $('#box10').hide();
+});
+
+
+$('#czechfemale').mouseenter(function() {
+    $('#box11').show();
+});
+
+$('#czechfemale').mouseleave(function() {
+    $('#box11').hide();
+});
+
+
+$('#denmarkmale').mouseenter(function() {
+    $('#box12').show();
+});
+
+$('#denmarkmale').mouseleave(function() {
+    $('#box12').hide();
+});
+
+$('#denmarkfemale').mouseenter(function() {
+    $('#box13').show();
+});
+
+$('#denmarkfemale').mouseleave(function() {
+    $('#box13').hide();
+});
+
+
+$('#estoniamale').mouseenter(function() {
+    $('#box14').show();
+});
+
+$('#estoniamale').mouseleave(function() {
+    $('#box14').hide();
+});
+
+
+$('#estoniafemale').mouseenter(function() {
+    $('#box15').show();
+});
+
+$('#estoniafemale').mouseleave(function() {
+    $('#box15').hide();
+});
+
+
+
+$('#finlandmale').mouseenter(function() {
+    $('#box16').show();
+});
+
+$('#finlandmale').mouseleave(function() {
+    $('#box16').hide();
+});
+
+$('#finlandfemale').mouseenter(function() {
+    $('#box17').show();
+});
+
+$('#finlandfemale').mouseleave(function() {
+    $('#box17').hide();
+});
+
+
+$('#francemale').mouseenter(function() {
+    $('#box18').show();
+});
+
+$('#francemale').mouseleave(function() {
+    $('#box18').hide();
+});
+
+
+$('#francefemale').mouseenter(function() {
+    $('#box19').show();
+});
+
+$('#francefemale').mouseleave(function() {
+    $('#box19').hide();
+});
+
+
+$('#germanymale').mouseenter(function() {
+    $('#box20').show();
+});
+
+$('#germanymale').mouseleave(function() {
+    $('#box20').hide();
+});
+
+$('#germanyfemale').mouseenter(function() {
+    $('#box21').show();
+});
+
+$('#germanyfemale').mouseleave(function() {
+    $('#box21').hide();
+});
+
+
+$('#hungmale').mouseenter(function() {
+    $('#box22').show();
+});
+
+$('#hungmale').mouseleave(function() {
+    $('#box22').hide();
+});
+
+$('#hungfemale').mouseenter(function() {
+    $('#box23').show();
+});
+
+$('#hungfemale').mouseleave(function() {
+    $('#box23').hide();
+});
+
+$('#icelandmale').mouseenter(function() {
+    $('#box24').show();
+});
+
+$('#icelandmale').mouseleave(function() {
+    $('#box24').hide();
+});
+
+$('#icelandfemale').mouseenter(function() {
+    $('#box25').show();
+});
+
+$('#icelandfemale').mouseleave(function() {
+    $('#box25').hide();
+});
+
+
+
+$('#irelandmale').mouseenter(function() {
+    $('#box26').show();
+});
+
+$('#irelandmale').mouseleave(function() {
+    $('#box26').hide();
+});
+
+$('#irelandfemale').mouseenter(function() {
+    $('#box27').show();
+});
+
+$('#irelandfemale').mouseleave(function() {
+    $('#box27').hide();
+});
+
+
+$('#italymale').mouseenter(function() {
+    $('#box28').show();
+});
+
+$('#italymale').mouseleave(function() {
+    $('#box28').hide();
+});
+
+$('#italyfemale').mouseenter(function() {
+    $('#box29').show();
+});
+
+$('#italyfemale').mouseleave(function() {
+    $('#box29').hide();
+});
+
+
+$('#latviamale').mouseenter(function() {
+    $('#box30').show();
+});
+
+$('#latviamale').mouseleave(function() {
+    $('#box30').hide();
+});
+
+$('#latviafemale').mouseenter(function() {
+    $('#box31').show();
+});
+
+$('#latviafemale').mouseleave(function() {
+    $('#box31').hide();
+});
+
+
+
+$('#lithmale').mouseenter(function() {
+    $('#box32').show();
+});
+
+$('#lithmale').mouseleave(function() {
+    $('#box32').hide();
+});
+
+$('#lithfemale').mouseenter(function() {
+    $('#box33').show();
+});
+
+$('#lithfemale').mouseleave(function() {
+    $('#box33').hide();
+});
+
+
+$('#luxemale').mouseenter(function() {
+    $('#box34').show();
+});
+
+$('#luxemale').mouseleave(function() {
+    $('#box34').hide();
+});
+
+$('#luxefemale').mouseenter(function() {
+    $('#box35').show();
+});
+
+$('#luxefemale').mouseleave(function() {
+    $('#box35').hide();
+});
+
+
+$('#maltamale').mouseenter(function() {
+    $('#box36').show();
+});
+
+$('#maltamale').mouseleave(function() {
+    $('#box36').hide();
+});
+
+$('#maltafemale').mouseenter(function() {
+    $('#box37').show();
+});
+
+$('#maltafemale').mouseleave(function() {
+    $('#box37').hide();
+});
+
+
+
+
+$('#nethemale').mouseenter(function() {
+    $('#box38').show();
+});
+
+$('#nethemale').mouseleave(function() {
+    $('#box38').hide();
+});
+
+$('#nethefemale').mouseenter(function() {
+    $('#box39').show();
+});
+
+$('#nethefemale').mouseleave(function() {
+    $('#box39').hide();
+});
+
+
+$('#norwaymale').mouseenter(function() {
+    $('#box40').show();
+});
+
+$('#norwaymale').mouseleave(function() {
+    $('#box40').hide();
+});
+
+$('#norwayfemale').mouseenter(function() {
+    $('#box41').show();
+});
+
+$('#norwayfemale').mouseleave(function() {
+    $('#box41').hide();
+});
+
+
+$('#polandmale').mouseenter(function() {
+    $('#box42').show();
+});
+
+$('#polandmale').mouseleave(function() {
+    $('#box42').hide();
+});
+
+$('#polandfemale').mouseenter(function() {
+    $('#box43').show();
+});
+
+$('#polandfemale').mouseleave(function() {
+    $('#box43').hide();
+});
+
+
+$('#portugalmale').mouseenter(function() {
+    $('#box44').show();
+});
+
+$('#portugalmale').mouseleave(function() {
+    $('#box44').hide();
+});
+
+$('#portugalfemale').mouseenter(function() {
+    $('#box45').show();
+});
+
+$('#portugalfemale').mouseleave(function() {
+    $('#box45').hide();
+});
+
+
+$('#romaniamale').mouseenter(function() {
+    $('#box46').show();
+});
+
+$('#romaniamale').mouseleave(function() {
+    $('#box46').hide();
+});
+
+$('#romaniafemale').mouseenter(function() {
+    $('#box47').show();
+});
+
+$('#romaniafemale').mouseleave(function() {
+    $('#box47').hide();
+});
+
+
+
+$('#slovakmale').mouseenter(function() {
+    $('#box48').show();
+});
+
+$('#slovakmale').mouseleave(function() {
+    $('#box48').hide();
+});
+
+$('#slovakfemale').mouseenter(function() {
+    $('#box49').show();
+});
+
+$('#slovakfemale').mouseleave(function() {
+    $('#box49').hide();
+});
+
+
+$('#slovenmale').mouseenter(function() {
+    $('#box50').show();
+});
+
+$('#slovenmale').mouseleave(function() {
+    $('#box50').hide();
+});
+
+$('#slovenfemale').mouseenter(function() {
+    $('#box51').show();
+});
+
+$('#slovenfemale').mouseleave(function() {
+    $('#box51').hide();
+});
+
+
+$('#spainmale').mouseenter(function() {
+    $('#box52').show();
+});
+
+$('#spainmale').mouseleave(function() {
+    $('#box52').hide();
+});
+
+$('#spainfemale').mouseenter(function() {
+    $('#box53').show();
+});
+
+$('#spainfemale').mouseleave(function() {
+    $('#box53').hide();
+});
+
+
+$('#swedenmale').mouseenter(function() {
+    $('#box54').show();
+});
+
+$('#swedenmale').mouseleave(function() {
+    $('#box54').hide();
+});
+
+$('#swedenfemale').mouseenter(function() {
+    $('#box55').show();
+});
+
+$('#swedenfemale').mouseleave(function() {
+    $('#box55').hide();
+});
+
+
+
+$('#ukmale').mouseenter(function() {
+    $('#box56').show();
+});
+
+$('#ukmale').mouseleave(function() {
+    $('#box56').hide();
+});
+
+$('#ukfemale').mouseenter(function() {
+    $('#box57').show();
+});
+
+$('#ukfemale').mouseleave(function() {
+    $('#box57').hide();
+});
+
+<!-- FUNZIONI GRAFICO FULL TIME -->
+
+$('#austriamale1').mouseenter(function() {
+    $('#box58').show();
+});
+
+$('#austriamale1').mouseleave(function() {
+    $('#box58').hide();
+});
+
+$('#austriafemale1').mouseenter(function() {
+    $('#box59').show();
+});
+
+$('#austriafemale1').mouseleave(function() {
+    $('#box59').hide();
+});
+
+
+
+$('#belgiummale1').mouseenter(function() {
+    $('#box60').show();
+});
+
+$('#belgiummale1').mouseleave(function() {
+    $('#box60').hide();
+});
+
+$('#belgiumfemale1').mouseenter(function() {
+    $('#box61').show();
+});
+
+$('#belgiumfemale1').mouseleave(function() {
+    $('#box61').hide();
+});
+
+
+$('#bulgmale1').mouseenter(function() {
+    $('#box62').show();
+});
+
+$('#bulgmale1').mouseleave(function() {
+    $('#box62').hide();
+});
+
+
+$('#bulgfemale1').mouseenter(function() {
+    $('#box63').show();
+});
+
+$('#bulgfemale1').mouseleave(function() {
+    $('#box63').hide();
+});
+
+
+$('#croatiamale1').mouseenter(function() {
+    $('#box64').show();
+});
+
+$('#croatiamale1').mouseleave(function() {
+    $('#box64').hide();
+});
+
+$('#croatiafemale1').mouseenter(function() {
+    $('#box65').show();
+});
+
+$('#croatiafemale1').mouseleave(function() {
+    $('#box65').hide();
+});
+
+
+$('#cyprusmale1').mouseenter(function() {
+    $('#box66').show();
+});
+
+$('#cyprusmale1').mouseleave(function() {
+    $('#box66').hide();
+});
+
+$('#cyprusfemale1').mouseenter(function() {
+    $('#box67').show();
+});
+
+$('#cyprusfemale1').mouseleave(function() {
+    $('#box67').hide();
+});
+
+
+$('#czechmale1').mouseenter(function() {
+    $('#box68').show();
+});
+
+$('#czechmale1').mouseleave(function() {
+    $('#box68').hide();
+});
+
+
+$('#czechfemale1').mouseenter(function() {
+    $('#box69').show();
+});
+
+$('#czechfemale1').mouseleave(function() {
+    $('#box69').hide();
+});
+
+
+$('#denmarkmale1').mouseenter(function() {
+    $('#box70').show();
+});
+
+$('#denmarkmale1').mouseleave(function() {
+    $('#box70').hide();
+});
+
+$('#denmarkfemale1').mouseenter(function() {
+    $('#box71').show();
+});
+
+$('#denmarkfemale1').mouseleave(function() {
+    $('#box71').hide();
+});
+
+
+$('#estoniamale1').mouseenter(function() {
+    $('#box72').show();
+});
+
+$('#estoniamale1').mouseleave(function() {
+    $('#box72').hide();
+});
+
+
+$('#estoniafemale1').mouseenter(function() {
+    $('#box73').show();
+});
+
+$('#estoniafemale1').mouseleave(function() {
+    $('#box73').hide();
+});
+
+
+
+$('#finlandmale1').mouseenter(function() {
+    $('#box74').show();
+});
+
+$('#finlandmale1').mouseleave(function() {
+    $('#box74').hide();
+});
+
+$('#finlandfemale1').mouseenter(function() {
+    $('#box75').show();
+});
+
+$('#finlandfemale1').mouseleave(function() {
+    $('#box75').hide();
+});
+
+
+$('#francemale1').mouseenter(function() {
+    $('#box76').show();
+});
+
+$('#francemale1').mouseleave(function() {
+    $('#box76').hide();
+});
+
+
+$('#francefemale1').mouseenter(function() {
+    $('#box77').show();
+});
+
+$('#francefemale1').mouseleave(function() {
+    $('#box77').hide();
+});
+
+
+$('#germanymale1').mouseenter(function() {
+    $('#box78').show();
+});
+
+$('#germanymale1').mouseleave(function() {
+    $('#box78').hide();
+});
+
+$('#germanyfemale1').mouseenter(function() {
+    $('#box79').show();
+});
+
+$('#germanyfemale1').mouseleave(function() {
+    $('#box79').hide();
+});
+
+
+$('#hungmale1').mouseenter(function() {
+    $('#box80').show();
+});
+
+$('#hungmale1').mouseleave(function() {
+    $('#box80').hide();
+});
+
+$('#hungfemale1').mouseenter(function() {
+    $('#box81').show();
+});
+
+$('#hungfemale1').mouseleave(function() {
+    $('#box81').hide();
+});
+
+$('#icelandmale1').mouseenter(function() {
+    $('#box82').show();
+});
+
+$('#icelandmale1').mouseleave(function() {
+    $('#box82').hide();
+});
+
+$('#icelandfemale1').mouseenter(function() {
+    $('#box83').show();
+});
+
+$('#icelandfemale1').mouseleave(function() {
+    $('#box83').hide();
+});
+
+
+
+$('#irelandmale1').mouseenter(function() {
+    $('#box84').show();
+});
+
+$('#irelandmale1').mouseleave(function() {
+    $('#box84').hide();
+});
+
+$('#irelandfemale1').mouseenter(function() {
+    $('#box85').show();
+});
+
+$('#irelandfemale1').mouseleave(function() {
+    $('#box85').hide();
+});
+
+
+$('#italymale1').mouseenter(function() {
+    $('#box86').show();
+});
+
+$('#italymale1').mouseleave(function() {
+    $('#box86').hide();
+});
+
+$('#italyfemale1').mouseenter(function() {
+    $('#box87').show();
+});
+
+$('#italyfemale1').mouseleave(function() {
+    $('#box87').hide();
+});
+
+
+$('#latviamale1').mouseenter(function() {
+    $('#box88').show();
+});
+
+$('#latviamale1').mouseleave(function() {
+    $('#box88').hide();
+});
+
+$('#latviafemale1').mouseenter(function() {
+    $('#box89').show();
+});
+
+$('#latviafemale1').mouseleave(function() {
+    $('#box89').hide();
+});
+
+
+
+$('#lithmale1').mouseenter(function() {
+    $('#box90').show();
+});
+
+$('#lithmale1').mouseleave(function() {
+    $('#box90').hide();
+});
+
+$('#lithfemale1').mouseenter(function() {
+    $('#box91').show();
+});
+
+$('#lithfemale1').mouseleave(function() {
+    $('#box91').hide();
+});
+
+
+$('#luxemale1').mouseenter(function() {
+    $('#box92').show();
+});
+
+$('#luxemale1').mouseleave(function() {
+    $('#box92').hide();
+});
+
+$('#luxefemale1').mouseenter(function() {
+    $('#box93').show();
+});
+
+$('#luxefemale1').mouseleave(function() {
+    $('#box93').hide();
+});
+
+
+$('#maltamale1').mouseenter(function() {
+    $('#box94').show();
+});
+
+$('#maltamale1').mouseleave(function() {
+    $('#box94').hide();
+});
+
+$('#maltafemale1').mouseenter(function() {
+    $('#box95').show();
+});
+
+$('#maltafemale1').mouseleave(function() {
+    $('#box95').hide();
+});
+
+
+
+
+$('#nethemale1').mouseenter(function() {
+    $('#box96').show();
+});
+
+$('#nethemale1').mouseleave(function() {
+    $('#box96').hide();
+});
+
+$('#nethefemale1').mouseenter(function() {
+    $('#box97').show();
+});
+
+$('#nethefemale1').mouseleave(function() {
+    $('#box97').hide();
+});
+
+
+$('#norwaymale1').mouseenter(function() {
+    $('#box98').show();
+});
+
+$('#norwaymale1').mouseleave(function() {
+    $('#box98').hide();
+});
+
+$('#norwayfemale1').mouseenter(function() {
+    $('#box99').show();
+});
+
+$('#norwayfemale1').mouseleave(function() {
+    $('#box99').hide();
+});
+
+
+$('#polandmale1').mouseenter(function() {
+    $('#box100').show();
+});
+
+$('#polandmale1').mouseleave(function() {
+    $('#box100').hide();
+});
+
+$('#polandfemale1').mouseenter(function() {
+    $('#box101').show();
+});
+
+$('#polandfemale1').mouseleave(function() {
+    $('#box101').hide();
+});
+
+
+$('#portugalmale1').mouseenter(function() {
+    $('#box102').show();
+});
+
+$('#portugalmale1').mouseleave(function() {
+    $('#box102').hide();
+});
+
+$('#portugalfemale1').mouseenter(function() {
+    $('#box103').show();
+});
+
+$('#portugalfemale1').mouseleave(function() {
+    $('#box103').hide();
+});
+
+
+$('#romaniamale1').mouseenter(function() {
+    $('#box104').show();
+});
+
+$('#romaniamale1').mouseleave(function() {
+    $('#box104').hide();
+});
+
+$('#romaniafemale1').mouseenter(function() {
+    $('#box105').show();
+});
+
+$('#romaniafemale1').mouseleave(function() {
+    $('#box105').hide();
+});
+
+
+
+$('#slovakmale1').mouseenter(function() {
+    $('#box106').show();
+});
+
+$('#slovakmale1').mouseleave(function() {
+    $('#box106').hide();
+});
+
+$('#slovakfemale1').mouseenter(function() {
+    $('#box107').show();
+});
+
+$('#slovakfemale1').mouseleave(function() {
+    $('#box107').hide();
+});
+
+
+$('#slovenmale1').mouseenter(function() {
+    $('#box108').show();
+});
+
+$('#slovenmale1').mouseleave(function() {
+    $('#box108').hide();
+});
+
+$('#slovenfemale1').mouseenter(function() {
+    $('#box109').show();
+});
+
+$('#slovenfemale1').mouseleave(function() {
+    $('#box109').hide();
+});
+
+
+$('#spainmale1').mouseenter(function() {
+    $('#box110').show();
+});
+
+$('#spainmale1').mouseleave(function() {
+    $('#box110').hide();
+});
+
+$('#spainfemale1').mouseenter(function() {
+    $('#box111').show();
+});
+
+$('#spainfemale1').mouseleave(function() {
+    $('#box111').hide();
+});
+
+
+$('#swedenmale1').mouseenter(function() {
+    $('#box112').show();
+});
+
+$('#swedenmale1').mouseleave(function() {
+    $('#box112').hide();
+});
+
+$('#swedenfemale1').mouseenter(function() {
+    $('#box113').show();
+});
+
+$('#swedenfemale1').mouseleave(function() {
+    $('#box113').hide();
+});
+
+
+
+$('#ukmale1').mouseenter(function() {
+    $('#box114').show();
+});
+
+$('#ukmale1').mouseleave(function() {
+    $('#box114').hide();
+});
+
+$('#ukfemale1').mouseenter(function() {
+    $('#box115').show();
+});
+
+$('#ukfemale1').mouseleave(function() {
+    $('#box115').hide();
+});
